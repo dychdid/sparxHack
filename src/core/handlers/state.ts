d555eeefdc8239a8d8f30041477b980f1872b1e8
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-10-13 13:57:42 -0700

    Initial Commit

--- src/core/handlers/state.ts
+++
@@ -1,7 +0,0 @@
-import StorageHandler from './storage';
-
-export const storages = {
-    colors: new StorageHandler('AzaleaCustomTheme', false),
-    preferences: new StorageHandler('AzaleaPreferences', false),
-    bookwork: new StorageHandler('AzaleaBookwork'),
-}
